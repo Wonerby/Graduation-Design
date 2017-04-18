fdec33a623ba54b069d3b349ea5bcb9e28b86160
Wonerby <wonerby@outlook.com>
2017-04-18 17:01:05 +0800

    again

--- MyMatlab/NBIImageDisplay.m
+++ MyMatlab/NBIImageDisplay.m
@@ -4,6 +4,8 @@ function output = doDisplay(FilePath)
     % Syntax: output = doDisplay(FilePath);//FilePath为图片路径
     %
     % 测试图像的显示
+    %
+    FilePath='‪C:\Users\Wonerby\Pictures\[三輪士郎]\4a547606df60402b030881c6.jpg';
     imdata=imread(FilePath);
     imshow(imdata);
 end
