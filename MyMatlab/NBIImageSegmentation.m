4a648e182c2ddd04e203b4f9e4c940a347c732c7
Wonerby <wonerby@outlook.com>
2017-04-18 16:30:58 +0800

    do some changes

--- MyMatlab/NBIImageSegmentation.m
+++ MyMatlab/NBIImageSegmentation.m
@@ -1,8 +1,8 @@
 function output = doSegmentation(FilePath)
-% doSegmentation - 执行分割
-%
-% Syntax: output = doSegmentation(FilePath);//FilePath为图片路径
-%
-% 执行图像分割的主程序
+    % doSegmentation - 执行分割
+    %
+    % Syntax: output = doSegmentation(FilePath);//FilePath为图片路径
+    %
+    % 执行图像分割的主程序
     
 end
